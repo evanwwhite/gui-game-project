ff124fbcbbbb5f2b161a2198b79dd3187a01c100
Evan White <69662443+evanwwhite@users.noreply.github.com>
2021-05-16 22:17:19 -0500

    Update MajorFinalProject.java

--- MajorFinalProject.java
+++ MajorFinalProject.java
@@ -12,13 +12,13 @@ public class MajorFinalProject extends JFrame implements ActionListener{
 
 	private JPanel mainPanel, sidePanel, topPanel, rightPanel;
 	private JRadioButton africa, europe, india, america;
-	private JButton nextButton, checkButton, exitButton, topButton, picButton;
+	private JButton nextButton, checkButton, exitButton, picButton;
 	private JTextField sideText, topText, checkText;
-	private ImageIcon pic1 = new ImageIcon("Sahara-desert.jpg");
+	private ImageIcon pic1 = new ImageIcon("sahara.jpg");
 	private ImageIcon pic2 = new ImageIcon("tower.jpg");
-	private ImageIcon pic3 = new ImageIcon("india-tajmahal.jpg");
+	private ImageIcon pic3 = new ImageIcon("tajmahal- india.jpg");
 	private ImageIcon pic4 = new ImageIcon("america.jpg");
-	private int counter = 0;
+	private int counter = 1;
 	
 	private ImageIcon[] Pics = {pic1, pic2, pic3, pic4 }; // remember to add at end
 	
@@ -47,7 +47,7 @@ public class MajorFinalProject extends JFrame implements ActionListener{
 
 		mainPanel = new JPanel();
 		mainPanel.setLayout(new BorderLayout());
-		mainPanel.setPreferredSize(new Dimension(800,650));
+		mainPanel.setPreferredSize(new Dimension(1000,650));
 		mainPanel.setBorder(BorderFactory.createMatteBorder(20,20,20,20, Color.WHITE));
 		
 //==//
@@ -72,7 +72,8 @@ public class MajorFinalProject extends JFrame implements ActionListener{
 		rightPanel.setBorder(BorderFactory.createMatteBorder(5,5,0,0, Color.WHITE));
 		
 //==//
-		 picButton = new JButton(Pics[0]);
+         picButton = new JButton(Pics[0]);
+         picButton.addActionListener(this);
 		 rightPanel.add(picButton);
 		 
 		ButtonGroup placeGroup = new ButtonGroup();
@@ -90,11 +91,12 @@ public class MajorFinalProject extends JFrame implements ActionListener{
 		sideText = new JTextField("        Where Am I ?");
 		sideText.setFont(new Font("Adobe Gothic Std B",0,15 ));
 		sidePanel.add(sideText);
-		sidePanel.add(europe);
-		sidePanel.add(africa);
-		sidePanel.add(india);
-		sidePanel.add(america);
 		
+        sidePanel.add(india);
+        sidePanel.add(europe);
+        sidePanel.add(america);
+        sidePanel.add(africa);
+	
 		africa.addActionListener(this);
 		europe.addActionListener(this);
 		india.addActionListener(this);
@@ -114,34 +116,78 @@ public class MajorFinalProject extends JFrame implements ActionListener{
 		
 //==//
 		 
-		topText = new JTextField(" Hello this is a Geography guessing game. This is where you will see if you are correct:");
+		topText = new JTextField("This is where you will see if you are correct:");
 		topText.setFont(new Font("Adobe Gothic Std B",0,14 ));
 		topText.setSize(500,100);
 		topPanel.add(topText);
 		
-		checkText = new JTextField("dlajd;flajs");
+		checkText = new JTextField("");
 		checkText.setSize(200,100);
 		topPanel.add(checkText);
 		
 //==//		
+
 		mainPanel.add(topPanel, BorderLayout.NORTH);
 		mainPanel.add(sidePanel, BorderLayout.WEST);
 		mainPanel.add(rightPanel, BorderLayout.CENTER);
 		setContentPane(mainPanel);
 	
-		
 	}
 	  public void actionPerformed(ActionEvent e)
 	  
 	  {        	
 	       if(e.getSource() == exitButton ) {
 	            System.exit(0);
-	      }
-	      else if(e.getSource() == nextButton){
-	          picButton.setIcon(Pics[counter]);
+           }
+          
+	       if(e.getSource() == nextButton){
+              picButton.setIcon(Pics[counter]);
 	          counter++;
 	          counter = counter%Pics.length;
-	      }
-	  }
+           }
+
+           if(e.getSource() == checkButton){
+
+              if(africa.isSelected() && counter == 1){
+                  checkText.setText("Yes!");
+              }
+              else if(europe.isSelected() && counter == 2){
+                checkText.setText("Yes!");
+              }
+              else if(india.isSelected() && counter == 3){
+                checkText.setText("Yes!");
+              }
+              else if(america.isSelected() && counter == 0){
+                checkText.setText("Yes!");
+              }
+              else{
+                  checkText.setText("No!");
+              }
+           }
+
+           if(e.getSource() == picButton){
+
+            try{
+                String fileName = "data.txt";
+                // Write file
+                FileWriter writer = new FileWriter(fileName);
+                BufferedWriter out = new BufferedWriter(writer);
+                String detail = "Hello You Found A Secret"; 
+                out.write("Time stamp: " + Calendar.getInstance().getTime());
+                out.newLine();
+                out.newLine();
+                out.write(String.format("Fun Detail: %-15s", detail));
+                out.close();
+                // Open file
+                File file = new File(fileName);
+                Desktop desktop = Desktop.getDesktop();
+                desktop.open(file);
+
+            } catch(Exception ex){
+                System.out.println("Error in writing to a text file.");
+            }
+        }
+    }
 }
+
 
